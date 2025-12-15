704ced04e2d4fefd23826139894d3b1edfa6320e
DUBORD Ruben <12300410@s30112.iutv.univ-paris13.fr>
2025-12-15 12:15:48 +0100

    Ajout d'un premier exemple de pokémon dans le pokédex. Ajout d'une couleur associée à chaque type.

---
+++ pokedex.py
@@ -0,0 +1,30 @@
+import pygame
+
+type_colors = {
+	'normal': '#A8A77A',
+	'feu': '#EE8130',
+	'eau': '#6390F0',
+	'electrik': '#F7D02C',
+	'plante': '#7AC74C',
+	'glace': '#96D9D6',
+	'combat': '#C22E28',
+	'poison': '#A33EA1',
+	'sol': '#E2BF65',
+	'vol': '#A98FF3',
+	'psy': '#F95587',
+	'insecte': '#A6B91A',
+	'roche': '#B6A136',
+	'spectre': '#735797',
+	'dragon': '#6F35FC',
+	'ténèbres': '#705746',
+	'acier': '#B7B7CE',
+	'fée': '#D685AD',
+}
+
+dico_pokemon = [{'number':'001',
+                 'name':'bulbizarre',
+                 'types':['plante','poison'],
+                 'size': '80',
+                 'weight': '30',
+                 'species': 'bulbe',
+                 'description':'bulle bizarre un peu nan'}]
